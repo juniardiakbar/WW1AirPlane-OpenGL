eab8020c766244ec8e04ab06de179531d62cc5bd
Juniardi Akbar <juniardiakbar@gmail.com>
2020-03-08 14:59:10 +0700

    Draw body, main wing, and propeller. Implement basic lighting

--- main.cpp
+++ main.cpp
@@ -7,11 +7,14 @@ using namespace std;
 
 #define SCALE 500
 
-int th = 0;
-int ph = 0;
+int th = 32;
+int ph = 8;
 int asp = 1;
+int rotate_speed = 25;
 float zoom = 1.0;
 float dim = 1.0;
+int theta = 0;
+string filename[] = {"main_wing.txt", "body.txt"};
 
 void tokenize(string const &str, const char delim, float out[])
 {
@@ -27,7 +30,7 @@ void tokenize(string const &str, const char delim, float out[])
     }
 }
 
-void init()
+void Initialize()
 {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -37,21 +40,95 @@ void init()
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
+    // Lighting set up
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    // Set lighting intensity and color
+    GLfloat qaAmbientLight[] = {0.2, 0.2, 0.2, 1.0};
+    GLfloat qaDiffuseLight[] = {0.8, 0.8, 0.8, 1.0};
+    GLfloat qaSpecularLight[] = {1.0, 1.0, 1.0, 1.0};
+    glLightfv(GL_LIGHT0, GL_AMBIENT, qaAmbientLight);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, qaDiffuseLight);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, qaSpecularLight);
+
+    // Set the light position
+    GLfloat qaLightPosition[] = {.5, .5, 0.0, 1.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, qaLightPosition);
 }
 
-void draw_plane(void)
+void idleCallBack()
+{
+    // Spins the cube around the set axis.
+    theta += rotate_speed;
+    theta %= 360;
+
+    glutPostRedisplay();
+}
+
+void Draw(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
+
+    GLfloat qaBlack[] = {0.0, 0.0, 0.0, 1.0};
+    GLfloat qaGreen[] = {0.0, 1.0, 0.0, 1.0};
+    GLfloat qaWhite[] = {1.0, 1.0, 1.0, 1.0};
+    glMaterialfv(GL_FRONT, GL_AMBIENT, qaGreen);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, qaGreen);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, qaWhite);
+    glMaterialf(GL_FRONT, GL_SHININESS, 60.0);
+
     glLoadIdentity();
 
     glScalef(zoom, zoom, 1);
     glRotatef(ph, 1, 0, 0);
     glRotatef(th, 0, 1, 0);
 
-    ifstream MyReadFile;
-    MyReadFile.open("main_wing.txt");
+    for (int i = 0; i < 2; i++)
+    {
+        ifstream MyReadFile;
+        MyReadFile.open(filename[i]);
 
+        while (!MyReadFile.eof())
+        {
+            string color, num_point;
+            float colors[3], points[3];
+
+            getline(MyReadFile, color);
+            tokenize(color, ' ', colors);
+
+            glBegin(GL_POLYGON);
+            glNormal3f(0.0, 0.0, 1.0);
+            glColor3f(colors[0], colors[1], colors[2]);
+
+            getline(MyReadFile, num_point);
+
+            int j = 0;
+            while (j < stoi(num_point))
+            {
+                string point;
+
+                getline(MyReadFile, point);
+                tokenize(point, ' ', points);
+                glVertex3f(points[0], points[1], points[2]);
+
+                j++;
+            }
+
+            glEnd();
+        }
+
+        MyReadFile.close();
+    }
+
+    // gluLookAt(1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glRotatef(theta, 1.0, 0.0, 0.0);
+
+    ifstream MyReadFile;
+    MyReadFile.open("propeller.txt");
     while (!MyReadFile.eof())
     {
         string color, num_point;
@@ -61,12 +138,13 @@ void draw_plane(void)
         tokenize(color, ' ', colors);
 
         glBegin(GL_POLYGON);
+        glNormal3f(0.0, 0.0, 1.0);
         glColor3f(colors[0], colors[1], colors[2]);
 
         getline(MyReadFile, num_point);
 
-        int i = 0;
-        while (i < stoi(num_point))
+        int j = 0;
+        while (j < stoi(num_point))
         {
             string point;
 
@@ -74,7 +152,7 @@ void draw_plane(void)
             tokenize(point, ' ', points);
             glVertex3f(points[0], points[1], points[2]);
 
-            i++;
+            j++;
         }
 
         glEnd();
@@ -85,7 +163,16 @@ void draw_plane(void)
     glutSwapBuffers();
 }
 
-void special_key(int key, int x, int y)
+// void DrawPropeller(void)
+// {
+//     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//     glEnable(GL_DEPTH_TEST);
+//     glLoadIdentity();
+
+//     glutSwapBuffers();
+// }
+
+void SpecialKey(int key, int x, int y)
 {
     switch (key)
     {
@@ -108,11 +195,11 @@ void special_key(int key, int x, int y)
     th %= 360;
     ph %= 360;
 
-    init();
+    Initialize();
     glutPostRedisplay();
 }
 
-void normal_key(unsigned char key, int x, int y)
+void NormalKey(unsigned char key, int x, int y)
 {
     switch (key)
     {
@@ -124,11 +211,15 @@ void normal_key(unsigned char key, int x, int y)
         if (zoom < 0)
             zoom = 0;
         break;
+    case 'r':
+        zoom = 1.0;
+        th = 32;
+        ph = 8;
     default:
         break;
     }
 
-    init();
+    Initialize();
     glutPostRedisplay();
 }
 
@@ -140,11 +231,13 @@ int main(int argc, char **argv)
     glutInitWindowSize(800, 800);
     glutCreateWindow("World War I Airplane Showcase");
 
-    init();
+    Initialize();
 
-    glutDisplayFunc(draw_plane);
-    glutSpecialFunc(special_key);
-    glutKeyboardFunc(normal_key);
+    glutDisplayFunc(Draw);
+    // glutDisplayFunc(DrawPropeller);
+    glutIdleFunc(idleCallBack);
+    glutSpecialFunc(SpecialKey);
+    glutKeyboardFunc(NormalKey);
 
     glutMainLoop();
 
